6d61ba5591cea4c868eb988442e3492105aa2d1d
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-06-11 16:27:09 +0000

    Deploying to gh-pages from @ ViktoriaTorianik/goit-js-hw-09@e2fb17e2bc7cf72d98a53ca164c2b0718c42cd5d 🚀

--- 01-color-switcher.2065ba7e.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((()=>{t.setAttribute("disabled","disabled"),r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.2065ba7e.js.map
--- 01-color-switcher.d58d32f1.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((function(){t.setAttribute("disabled","disabled"),r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.removeAttribute("disabled")}))}();
-//# sourceMappingURL=01-color-switcher.d58d32f1.js.map
