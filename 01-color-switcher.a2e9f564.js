e62fed93342f6ee2f45e2d6037a411a5f0cc1b56
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-06-11 16:11:38 +0000

    Deploying to gh-pages from @ ViktoriaTorianik/goit-js-hw-09@04624d410bc7a82371877262692952502f055711 🚀

--- 01-color-switcher.a2e9f564.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=document.querySelector("body");e.setAttribute("disabled","disabled"),t.addEventListener("click",(function(){timerId=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled","disabled"),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(timerId),t.removeAttribute("disabled"),e.setAttribute("disabled","disabled")}));
-//# sourceMappingURL=01-color-switcher.a2e9f564.js.map
