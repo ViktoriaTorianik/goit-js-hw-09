e2fb17e2bc7cf72d98a53ca164c2b0718c42cd5d
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-06-11 19:26:42 +0300

    задача 1 кнопка стоп на початку не активна

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,12 +2,15 @@ const startButton = document.querySelector("[data-start]")
 const stopButton = document.querySelector("[data-stop]")
 const colorBody = document.querySelector("body")
 
+stopButton.setAttribute("disabled", "")
+
 startButton.addEventListener('click', onStart)
 
 function onStart(){
     
     timerId = setInterval(()=> {
-        startButton.setAttribute("disabled", "disabled")
+        startButton.setAttribute("disabled", "")
+        stopButton.removeAttribute("disabled")
         colorBody.style.backgroundColor = getRandomHexColor()
     function getRandomHexColor() {
         return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
@@ -20,6 +23,7 @@ function onStop(){
     
     clearInterval(timerId)
     startButton.removeAttribute("disabled")
+    stopButton.setAttribute("disabled", "")
 }
 
 
