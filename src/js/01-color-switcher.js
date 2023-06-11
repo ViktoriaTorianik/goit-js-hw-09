86fb77fe2aa35bc2ebc3627c667b95cc22afb163
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-06-11 19:01:51 +0300

    старт-стоп

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,7 @@ const startButton = document.querySelector("[data-start]")
 const stopButton = document.querySelector("[data-stop]")
 const colorBody = document.querySelector("body")
 
-stopButton.setAttribute('disabled',"")
+stopButton.setAttribute("disabled", "disabled")
 
 startButton.addEventListener('click', onStart)
 
@@ -13,7 +13,7 @@ function onStart(){
     function getRandomHexColor() {
         return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
       } },1000)
-      startButton.setAttribute('disabled', "disabled")
+      startButton.setAttribute("disabled", "disabled")
       stopButton.removeAttribute('disabled')
     }
 
@@ -21,8 +21,8 @@ stopButton.addEventListener('click', onStop)
 function onStop(){
     
     clearInterval(timerId)
-    startButton.removeAttribute('disabled')
-    stopButton.setAttribute('disabled', "disabled")
+    startButton.removeAttribute("disabled")
+    stopButton.setAttribute("disabled", "disabled")
 }
 
 
