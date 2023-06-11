f5ce753afb19aa985ae151c2c902437686798d74
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-06-11 15:42:04 +0000

    Deploying to gh-pages from @ ViktoriaTorianik/goit-js-hw-09@317fb4af3f621add86ea4e0aeefeb7c75df96857 🚀

--- 03-promises.96d14f2f.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector(".form");console.log(o),console.log("vvvv")}();
-//# sourceMappingURL=03-promises.96d14f2f.js.map
--- 03-promises.d9b360f7.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".form");console.log(o),console.log("vvvv");
-//# sourceMappingURL=03-promises.d9b360f7.js.map
