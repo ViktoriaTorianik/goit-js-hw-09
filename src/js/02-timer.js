c5c827c2f3d986a57737d629cf03c0fff4327724
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-06-12 16:30:04 +0300

    update 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,71 +5,71 @@ import "flatpickr/dist/flatpickr.min.css";
 
 const inputEl = document.querySelector("#datetime-picker")
 const btn = document.querySelector("button")
-const days = document.querySelector('span[data-days]')
-const hours = document.querySelector('span[data-hours]')
-const minutes = document.querySelector('span[data-minutes]')
-const seconds = document.querySelector('span[data-seconds]')
+const day = document.querySelector('span[data-days]')
+const hour = document.querySelector('span[data-hours]')
+const minute = document.querySelector('span[data-minutes]')
+const second = document.querySelector('span[data-seconds]')
 const divTimer = document.querySelector("div")
 console.log(divTimer);
 
 divTimer.style.display = "flex"
 
- flatpickr(inputEl, {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        if(selectedDates[0]<new Date()){
-            window.alert("Please choose a date in the future")
-        } else {
-            btn.disabled = false
-            btn.addEventListener('click', onStartTime)
+btn.setAttribute('disabled',"")
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      if(selectedDates[0]<new Date()){
+          window.alert("Please choose a date in the future")
+      } else {
+          btn.disabled = false}
+      }}
+
+const flatpickrEl = flatpickr(inputEl, options);
+let timerId = null;
+ btn.addEventListener('click', onStartTime)
   function onStartTime(){
- const timerId = setInterval(()=>{
+ timerId = setInterval(()=>{
     const currentTime = new Date
-    const deltaTime = selectedDates[0] - currentTime 
-    const finishTime = convertMs(deltaTime)
+    const deltaTime = flatpickrEl.selectedDates[0].getTime() - currentTime.getTime() 
+    convertMs(deltaTime); 
     
-    days.textContent = addLeadingZero(finishTime.days);
-    hours.textContent = addLeadingZero(finishTime.hours);
-    minutes.textContent = addLeadingZero(finishTime.minutes);
-    seconds.textContent = addLeadingZero(finishTime.seconds);
-    
-  },1000)
-  if(selectedDates[0] <= 0){
-    clearInterval(timerId)
-  }
-  }
-        }
-      console.log(selectedDates[0]);
-    },
-  })
-  btn.setAttribute('disabled',"")
-  
+  },1000);
  
+  }
+
   function convertMs(ms) {
-    // Number of milliseconds per unit of time
+    
+    function addLeadingZero(value) {
+      return `${value}`.padStart(2, '0');
+    }
+  
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
   
     // Remaining days
-    const days = Math.floor(ms / day);
+    const days = addLeadingZero(Math.floor(ms / day));
     // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
+    const hours =addLeadingZero(Math.floor((ms % day) / hour));
     // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
     // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+    
+    if (days === '00' && hours === '00' && minutes === '00' && seconds === '00') {
+      clearTimeout(timerId); 
+
+    day.textContent = days;
+    hour.textContent = hours;
+    minute.textContent = minutes;
+    second.textContent = seconds;
     return { days, hours, minutes, seconds };
   }
-  function addLeadingZero(value) {
-    return `${value}`.padStart(2, '0');
-  }
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+ 
+} 
  
